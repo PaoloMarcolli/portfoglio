5a2d4afd0465f1e9e298ec3eb84cfaaebbafe2fa
Paolo Marcolli <paolo.marcolli@hotmail.com>
2024-10-13 20:06:27 +0200

    aggiunto EmailJS

--- asset/js/script.js
+++ asset/js/script.js
@@ -20,3 +20,21 @@ $('#contact-form').on('submit', function(event) {
     });
 });
 // code fragment
+
+emailjs.init({
+    publicKey: 'GnTnGDNVDPIgO3pM3',
+    // Do not allow headless browsers
+    blockHeadless: true,
+    blockList: {
+      // Block the suspended emails
+      list: ['foo@emailjs.com', 'bar@emailjs.com'],
+      // The variable contains the email address
+      watchVariable: 'paolo.marcolli@hotmail.com',
+    },
+    limitRate: {
+      // Set the limit rate for the application
+      id: 'app',
+      // Allow 1 request per 10s
+      throttle: 10000,
+    },
+  });
