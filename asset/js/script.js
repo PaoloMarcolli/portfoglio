9238d3973712b538fa5621c06b77a6aef6e33118
Paolo Marcolli <paolo.marcolli@hotmail.com>
2024-10-22 18:25:05 +0200

    add curriculum vitae page

--- asset/js/script.js
+++ asset/js/script.js
@@ -26,3 +26,33 @@ window.onload = function() {
       document.getElementById("subject").value = "";
   });
 }
+function openWorkSheet(evt, woorkSheetName) {
+  var i, tabcontent, tablinks;
+  tabcontent = document.getElementsByClassName("cnt-woork-sheet");
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+  }
+  tablinks = document.getElementsByClassName("tablinks");
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace(" active", "");
+  }
+  document.getElementById(woorkSheetName).style.display = "block";
+  evt.currentTarget.className += " active";
+}
+
+function openWorkSheetPeriod(evt, woorkSheetPeriodName) {
+  var i, tabcontent, tablinks;
+  tabcontent = document.getElementsByClassName("cnt-woork-period");
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+  }
+  tablinks = document.getElementsByClassName("tablinks");
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace(" active", "");
+  }
+  document.getElementById(woorkSheetPeriodName).style.display = "block";
+  evt.currentTarget.className += " active";
+}
+// Get the element with id="defaultOpen" and click on it
+document.getElementById("defaultOpenWorkSheet").click();
+document.getElementById("defaultOpenWorkPeriod").click();
